f11f4316aa8847c10236c162ac0fba980cbd370a
dmurail <murali.dm@gmail.com>
2017-03-08 17:57:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 var counter = 0;
 app.get('/counter', function (req, res) {
     counter = counter + 1;
-  res.send(counter.toSrting());
+  res.send(counter.toString());
 });
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
